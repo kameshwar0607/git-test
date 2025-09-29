86261d356b64786b25127e41c72f9072c712dfa7
kameshwar0607 <kameshwarsingh9402@gmail.com>
2025-09-30 02:03:58 +0530

    final release

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
-# comment addes line 1
-a=[1,2,3,4,5,6,7,8,9,10]
-for i in a :
-    print(f'value from the list is {i}')
-# comment added line 2
-print("hello world")
-# comment added line 3
-print("line one in main.py")
+def main():
+    print("Hello, World!")
+    # comment addes line 1
+    a=[1,2,3,4,5,6,7,8,9,10]
+    for i in a :
+        print(f'value from the list is {i}')
+    # comment added line 2
+    print("hello world")
+    # comment added line 3
+    print("line one in main.py")
 
-# comment added line 4
-print("line three  in main.py")
+    # comment added line 4
+    print("line three  in main.py")
 
-# comment added line 6
-print("line 6th in main.py")
+    # comment added line 6
+    print("line 6th in main.py")
 
-#git push final commit
-# comment added line 7
-print("line 7th in main.py")            
-# comment added line 8
-print("line 8th in main.py")
-# comment added line 9
-print("line 9th in main.py")
+    #git push final commit
+    # comment added line 7
+    print("line 7th in main.py")           
+    # comment added line 8
+    print("line 8th in main.py")
+    # comment added line 9
+    print("line 9th in main.py")
+
+    # comment addes line 1
+    a=[1,2,3,4,5,6,7,8,9,10]
+    for i in a :
+        print(f'value from the list is {i}')
+    # comment added line 2
+    print("hello world")
+    # comment added line 3
+    print("line one in main.py")
+
+    # comment added line 4
+    print("line three  in main.py")
+
+    # comment added line 6
+    print("line 6th in main.py")
+
+    #git push final commit
+    # comment added line 7
+    print("line 7th in main.py")           
+    # comment added line 8
+    print("line 8th in main.py")
+    # comment added line 9
+    print("line 9th in main.py")
+if __name__ == "__main__":
+    main()
